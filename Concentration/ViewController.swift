fd30e784e239335532634aa5c4ce6a4746bdc3fd
Jodi Smit <jodi@nebulahome.com>
2019-04-04 16:45:23 -0400

    Added Model for MVC.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,13 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+	lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+	var flipCount = 0 {
+		didSet {
+			flipCountLabel.text = "Flips: \(flipCount)"
+		}
+	}
+	var emojiChoices = ["👹", "👺", "😈", "☠️", "🤖", "👽", "👾", "🎃", "🧟‍♂️"]
+	var emoji = [Int: String]()
+	
+	@IBOutlet var cardButtons: [UIButton]!
+	
+	@IBOutlet weak var flipCountLabel: UILabel!
+	
 	@IBAction func touchCard(_ sender: UIButton) {
-		flipCard(withEmoji: "👺", on: sender)
+		flipCount += 1
+		if let cardNumber = cardButtons.firstIndex(of: sender) {
+			game.chooseCard(at: cardNumber)
+			updateViewFromModel()
+		} else {
+			print("Card not in valid range")
+		}
 	}
 	
-	func flipCard(withEmoji emoji: String, on button: UIButton) {
+	func updateViewFromModel() {
+		for index in cardButtons.indices {
+			let button = cardButtons[index]
+			let card = game.cards[index]
+			if card.isFaceUp {
+				button.setTitle(emoji(for: card), for: .normal)
+				button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+			} else {
+				button.setTitle("", for: .normal)
+				button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)
+			}
+		}
+	}
+	
+	func emoji(for card: Card) -> String {
+		if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+			let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+			emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+		}
 		
+		
+		return emoji[card.identifier] ?? "?"
 	}
 }
 
